fa505081c1b73470051919e74dfab01f27b6e769
Cameron Graham <cameron.graham@foxconn4tech.com>
2019-03-07 16:28:00 +0100

    header and blog-card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React, { Component } from "react";
+import { compose } from 'redux'
 import { withFirebase } from "components/firebase/context";
+import Header from "components/UI/Header";
+import Grid from "@material-ui/core/Grid";
+import Blog from "components/UI/Blog";
+import { withStyles } from "@material-ui/core";
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '100px'
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  }
+});
 
 class App extends Component {
   state = {
@@ -12,6 +29,7 @@ class App extends Component {
       .orderByChild("deleted")
       .equalTo(false)
       .on("value", snapshot => {
+        console.log(snapshot.val());
         this.setState({
           blogs: snapshot.val()
         });
@@ -19,16 +37,24 @@ class App extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
-        <ul>
+        <Header />
+        <Grid container className={classes.root} >
           {Object.keys(this.state.blogs).map((blog, index) => {
-            return <li key={index}>{this.state.blogs[blog].title}</li>;
+            return (
+              <Grid item xs={12} sm={6} lg={4} xl={3} key={index}>
+                <Blog blog={this.state.blogs[blog]} />
+              </Grid>
+            );
           })}
-        </ul>
+        </Grid>
+        <ul />
       </div>
     );
   }
 }
 
-export default withFirebase(App);
+export default compose(withFirebase, withStyles(styles))(App);
---
+++ src/components/UI/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import blue from '@material-ui/core/colors/blue';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    wrapper: {
+        margin: theme.spacing.unit,
+        position: 'relative',
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    buttonProgress: {
+        color: blue[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
+});
+
+const Header = props => {
+    const { classes} = props;
+
+    return (
+        <AppBar className={classes.root}>
+            <Toolbar>
+                <IconButton
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="Menu"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    color="inherit"
+                    className={classes.grow}
+                >
+                    My Blog
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default withStyles(styles)(Header);
